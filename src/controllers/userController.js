9b44668c70fa844f1d8f8d845639c6356bdda42c
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-05-11 16:51:16 -0500

    update controllers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,27 +1,39 @@
 const User = require('../models/User')
 
-// Lo utilizaremos para crear usuarios
 const createUser = async (req, res) => {
-  const user = await User.create(req.body)
-  res.status(201).json(user)
+  await User.create(req.body)
+
+  if (req.headers.accept?.includes('text/html')) {
+    const users = await User.find()
+    res.render('usuarios', { users })
+  } else {
+    const users = await User.find()
+    res.status(201).json(users)
+  }
 }
 
-// Lo utilizaremos para obtener todos los usurios
 const getUsers = async (req, res) => {
   const users = await User.find()
   res.json(users)
 }
 
-// Lo utilizaremos para actualizar usuarios
 const updateUser = async (req, res) => {
   const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
   res.json(user)
 }
 
-// Lo utilizaremos para actualizar usuarios eliminar
 const deleteUser = async (req, res) => {
   await User.findByIdAndDelete(req.params.id)
   res.json({ message: 'Usuario eliminado' })
+}
+
+const showForm = (req, res) => {
+  res.render('formulario')
+}
+
+const listUsers = async (req, res) => {
+  const users = await User.find()
+  res.render('usuarios', { users })
 }
 
 module.exports = {
@@ -29,4 +41,6 @@ module.exports = {
   getUsers,
   updateUser,
   deleteUser,
+  showForm,
+  listUsers,
 }
