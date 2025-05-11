e8b382f3cc9fee1f59918da14bfa4eb8ea3d4e9b
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-05-11 16:51:07 -0500

    update routes add new routes form and users

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,11 +6,17 @@ const {
   getUsers,
   updateUser,
   deleteUser,
+  showForm,
+  listUsers
 } = require('../controllers/userController')
 
 router.post('/', createUser)
 router.get('/', getUsers)
 router.put('/:id', updateUser)
 router.delete('/:id', deleteUser)
+
+router.get('/formulario', showForm)
+router.post('/formulario', createUser) 
+router.get('/usuarios', listUsers)
 
 module.exports = router
