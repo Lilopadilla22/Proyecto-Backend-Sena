daa00a91bc9c757792f709dfc666a57067ab9cf9
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-05-11 16:51:27 -0500

    update index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const cors = require('cors')
 const dotenv = require('dotenv')
+const cors = require('cors')
 const connectDB = require('./config/db')
 const userRoutes = require('./routes/userRoutes')
 
@@ -8,11 +8,14 @@ dotenv.config()
 connectDB()
 
 const app = express()
+app.set('view engine', 'ejs')
+app.set('views', './src/views')
 
 app.use(cors())
+app.use(express.urlencoded({ extended: true })) 
 app.use(express.json())
-app.use('/api/users', userRoutes)
 
+app.use('/', userRoutes)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`🟢 Servidor en puerto ${PORT}`))
